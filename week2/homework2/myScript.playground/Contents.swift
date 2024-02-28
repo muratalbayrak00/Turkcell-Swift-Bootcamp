db2541f662ddc0702dd293ec2505bd4ac8fc4e34
Murat ALBAYRAK <muratalbayrak236@gmail.com>
2024-02-28 14:38:52 +0300

    git project euler 5 solved

--- week2/homework2/myScript.playground/Contents.swift
+++ week2/homework2/myScript.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
-// Week2 Homework 2
+// ------------------------   Week2 Homework 2 -----------------------------
 
-/* Project Euler 4
+/* => Project Euler 4
 
  A palindromic number reads the same both ways. The largest palindrome
  made from the product of two 2-digit numbers is 9009 = 91 × 99.
@@ -39,20 +39,43 @@ func isPalindrome(_ product: Int) -> Bool {
 
 
 
-
-
-
-
-
-/* Project Euler 5
+/* =>  Project Euler 5
 
  2520 is the smallest number that can be divided by each of the 
  numbers from 1 to 10 without any remainder.
 
  What is the smallest positive number that is evenly divisible 
- by all of the numbers from 1 to n?
+ by all of the numbers from 1 to n? */
 
 
+var number = 20
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    var a = a
+    var b = b
+    
+    while b != 0 {
+        let temp = b
+        b = a % b
+        a = temp
+    }
+    
+    return a
+    
+}
 
 
-*/
+func lcm(_ a: Int, _ b: Int) -> Int {
+    return a / gcd(a,b) * b
+}
+
+
+func smallestMultiple(_ n: Int) -> Int {
+    var result = 1
+    for i in 2...n {
+        result = lcm(result, i)
+    }
+    return result
+}
+
+smallestMultiple(number)
