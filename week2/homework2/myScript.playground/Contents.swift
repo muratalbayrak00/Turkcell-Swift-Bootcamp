1d56d16cdf9f565998671ac3304e9be80e53e86c
Murat ALBAYRAK <muratalbayrak236@gmail.com>
2024-02-28 10:42:21 +0300

    homework2 added

---
+++ week2/homework2/myScript.playground/Contents.swift
@@ -0,0 +1,58 @@
+import Foundation
+// Week2 Homework 2
+
+/* Project Euler 4
+
+ A palindromic number reads the same both ways. The largest palindrome
+ made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+ Find the largest palindrome made from the product of two n-digit numbers.
+ 
+ */
+
+
+var n = 2
+var nDigit = Int(pow(10, Double(n))-1)
+var largestNumber = 0
+for i in (0...nDigit).reversed() {
+    
+    for j in (0...nDigit).reversed() {
+        var product = i * j
+        
+        if isPalindrome(product) && product > largestNumber {
+            largestNumber = product
+            
+            
+        }
+        if i == 0 {
+            break
+        }
+    }
+}
+print(largestNumber)
+
+
+func isPalindrome(_ product: Int) -> Bool {
+    let numberString = String(product)
+    return numberString == String(numberString.reversed())
+}
+
+
+
+
+
+
+
+
+/* Project Euler 5
+
+ 2520 is the smallest number that can be divided by each of the 
+ numbers from 1 to 10 without any remainder.
+
+ What is the smallest positive number that is evenly divisible 
+ by all of the numbers from 1 to n?
+
+
+
+
+*/
--- week2/myScript.playground/Contents.swift
+++ week2/myScript.playground/Contents.swift
@@ -1,3 +1,111 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Tuple
+
+
+let tuple = (404, "Not found")
+
+print(tuple)
+print(tuple.0)
+print(tuple.1)
+
+
+// Isimli Tuple
+
+var namesTuple = (first: 1, middle: 2, last: 3)
+print(namesTuple.first)
+
+// Sonradan deger belirlenen tuple
+
+var numbertuple: (optionalFirst: Int?, middle: String, last: Int)?
+numbertuple = (nil, "cemalhan", last: 21)
+numbertuple?.last
+
+// Soru: Merkezi (0,0) olan ve yaricapi bir olan bir cember tanimlayiniz.
+
+typealias Circle = (center: (x: CGFloat, y: CGFloat), radius: CGFloat)
+var unitCircle: Circle = ((0.0, 0.0), 1)
+
+var a1 = 3
+var b1 = 4
+// swap yapma
+
+var temp = a1
+a1 = b1
+b1 = temp
+
+// kisa yollu swap islemi(tuple kullanilmali
+(a1, b1) = (b1, a1)
+
+// Tuples in switch
+let switchTuple = (firstCase: true, secondCase: false)
+
+switch switchTuple{
+case (true, true):
+    print("ikiside dogru")
+case (true, false)
+    print("birincisi dogru ikincisi yanlis")
+case (false, true):
+    print("birincisi yanlis ikincisi dogru")
+case (false, false):
+    print("ikiside yanlis")
+}
+
+
+//------------------------------------------------------------------------------------------------------------------
+
+// Array - Dizi
+// Ayni veri turunde olan degerleri tutar ancak istenirse farkli turleride tutabiliriz.
+//kullanmak icin as [Any] kullanilmali
+var cars = ["Dacia", 3] as [Any]
+
+var cities: [String] = []  // Bos string array
+var cities2: Array<String> = Array() // Bos string array
+var cities3 = Array<String>() // bos string array
+var cities4 = [String]() // bos string array
+
+// Arraye eleman ekleme ve indis kavrami
+
+cities.append("Malatya")
+cities.append("Sinop")
+cities.append("Bursa")
+cities.append("Baku")
+cities.append("Kahramanmaras")
+cities.append("Denizli")
+cities.append("Cannekkale")
+cities.append("Tekirdag")
+cities.append("Hatay")
+
+cities.first
+cities[0]
+cities.endIndex
+cities.last
+cities[cities.endIndex - 1]
+cities[cities.count - 1]
+
+var stringArray = Array(repeating: "Malatya", count: 4)
+
+// Array icinde array
+
+let matrix = [["a", "b", "c"], [1, 2, 3]]
+matrix[0][2]
+
+matrix.first?.last
+
+// Numerik Diziler
+
+var numberArray = [1, 2, 3]
+numberArray.max()
+numberArray.min()
+
+for city in cities {
+    print(city)
+}
+
+
+
+
+
+
+
+
