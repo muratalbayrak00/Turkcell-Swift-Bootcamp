7ad04b45f0c920ff23903a30ead60f38b09aff85
Murat ALBAYRAK <muratalbayrak236@gmail.com>
2024-03-19 05:09:17 +0300

    pageConroller added

---
+++ TicketApp/FirstViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FirstViewController.swift
+//  TicketApp
+//
+//  Created by murat albayrak on 19.03.2024.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var buttonNextPage: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buttonNextPage.setTitle("Diğer Sayfaya Git", for: .normal)
+        buttonNextPage.addTarget(self, action: #selector(goToSecondPage), for: .touchUpInside)
+      
+    }
+    
+    @objc func goToSecondPage() {
+            // İkinci sayfayı storyboard'dan al
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let secondVC = storyboard.instantiateViewController(withIdentifier: "DateController") as! DateController
+            
+            // Geçiş işlemini gerçekleştir
+            navigationController?.pushViewController(secondVC, animated: true)
+            //present(secondVC, animated: true, completion: nil)
+        // veya:
+            // present(secondVC, animated: true, completion: nil)
+        }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
